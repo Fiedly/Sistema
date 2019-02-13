288ee14f52937fe75960fe6bc1fa5b85d3aa9620
Pedro Asensio Martinez <Pedro.91am@gmail.com>
2019-02-13 01:41:35 +0100

    a

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
             <v-list-tile slot="activator">
               <v-list-tile-content>
                 <v-list-tile-title>
-                  Almacén2
+                  Almacén
                 </v-list-tile-title>
               </v-list-tile-content>
             </v-list-tile>
